a07021956d7d9c1954b1e6bc3135ebde67ecc375
gamez2riches-debug <gamez2riches@gmail.com>
2025-12-09 11:46:57 -0600

    Create Quiz.jsx

---
+++ src/pages/Quiz.jsx
@@ -0,0 +1,9 @@
+export default function Quiz() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Financial IQ Quiz</h1>
+      <p>Ready to discover your financial strengths?</p>
+      <p>(Quiz questions coming next.)</p>
+    </div>
+  );
+}
