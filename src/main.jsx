39d8fc51edbcd7fd08bc2c047993714b03a0e0e6
gamez2riches-debug <gamez2riches@gmail.com>
2025-12-09 12:02:51 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import Quiz from "./pages/Quiz";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
