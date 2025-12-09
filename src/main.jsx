0d5a1aa36c7ab197d05db893b92f8156b66916ef
gamez2riches-debug <gamez2riches@gmail.com>
2025-12-09 11:49:20 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Quiz from "./pages/Quiz";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/quiz" element={<Quiz />} />
+    </Routes>
+  </BrowserRouter>
 );
